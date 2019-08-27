791209bc361f52659d9957b0b9729341ac09cc7e
Jimmy Janssen <jjanssen@CSMBP0010-JJ.local>
2019-08-27 11:10:58 -0400

    fixing auth data call

--- src/config/authorization.ts
+++ src/config/authorization.ts
@@ -1,9 +1,11 @@
 import { IAuthData } from "../graph/auth/auth.interface";
 
 export function authorize(authData: IAuthData, roles?: string[]): void {
+    if (!authData) throw new Error('Unauthorized.');
     if ((!roles || !roles.length) && authData.userInfo) return;
     if (!Object.keys(authData.roles)
         .find(role =>
             roles.indexOf(authData.roles[role]) > -1))
         throw new Error('Unauthorized.');
+
 }
