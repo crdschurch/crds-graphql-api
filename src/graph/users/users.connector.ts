57e21e981b1c943d7902b234e428ca724aee96a5
Jimmy Janssen <jjanssen@CSMBP0010-JJ.local>
2019-09-03 14:42:41 -0400

    adding roles and types for groups

--- src/graph/users/users.connector.ts
+++ src/graph/users/users.connector.ts
@@ -2,7 +2,7 @@ import { injectable, inject } from "inversify";
 import { ISite } from "../sites/sites.interface";
 import { IGroup } from "../groups/groups.interface";
 import { IUsersConnector, IUser } from "./users.interface";
-import { Mongo } from '../../sources/mongo';
+import { Mongo } from "../../sources/mongo";
 import { ILifeStage } from "../content/contentTypes/lifeStage/lifeStage.interface";
 import { Types } from "../../ioc/types";
 import { IContact } from "./contact/contact.interface";
@@ -11,105 +11,121 @@ const MP = require("ministry-platform");
 
 @injectable()
 export class UsersConnector implements IUsersConnector {
+  constructor(@inject(Types.Mongo) private mongo: Mongo) {}
 
-    constructor(@inject(Types.Mongo) private mongo: Mongo) { }
+  public getCongregation(HouseholdID: number): Promise<ISite> {
+    const filter = `Households.[Household_ID] = ${HouseholdID}`;
+    const table = "Households";
+    const mp = new MP();
+    return mp
+      .withSelectColumns([
+        "Congregation_ID_Table.[Congregation_ID]",
+        "Congregation_ID_Table.[Congregation_Name]"
+      ])
+      .withFilter(filter)
+      .fromTable(table)
+      .get()
+      .then(response => {
+        return {
+          id: response.data[0].Congregation_ID,
+          name: response.data[0].Congregation_Name
+        };
+      });
+  }
 
-    public getCongregation(HouseholdID: number): Promise<ISite> {
-        const filter = `Households.[Household_ID] = ${HouseholdID}`;
-        const table = "Households";
-        const mp = new MP();
-        return mp
-            .withSelectColumns([
-                "Congregation_ID_Table.[Congregation_ID]",
-                "Congregation_ID_Table.[Congregation_Name]"
-            ])
-            .withFilter(filter)
-            .fromTable(table)
-            .get()
-            .then(response => {
-                return {
-                    id: response.data[0].Congregation_ID,
-                    name: response.data[0].Congregation_Name
-                };
-            });
-    }
+  public getGroups(UserID: number): Promise<IGroup[]> {
+    const filter = `Group_Participants.[Participant_ID] = ${UserID}`;
+    const table = "Group_Participants";
+    const mp = new MP();
+    return mp
+      .withSelectColumns([
+        "Group_Participants.[Group_Role_ID] as GroupRoleID",
+        "Group_Role_ID_Table.[Role_Title] as RoleTitle",
+        "Group_ID_Table.[Group_ID]",
+        "Group_ID_Table.[Group_Name]",
+        "Group_ID_Table.[Group_Type_ID] as GroupTypeID",
+        "Group_ID_Table_Group_Type_ID_Table.[Group_Type] as GroupTypeName"
+      ])
+      .withFilter(filter)
+      .fromTable(table)
+      .get()
+      .then(response => {
+        return response.data.map(group => {
+          return {
+            id: group.Group_ID,
+            name: group.Group_Name,
+            role: {
+              id: group.GroupRoleID,
+              name: group.RoleTitle
+            },
+            type: {
+              id: group.GroupTypeID,
+              name: group.GroupTypeName
+            }
+          };
+        });
+      });
+  }
 
-    public getGroups(UserID: number): Promise<IGroup[]> {
-        const filter = `Group_Participants.[Participant_ID] = ${UserID}`;
-        const table = "Group_Participants";
-        const mp = new MP();
-        return mp
-            .withSelectColumns([
-                "Group_Participants.[Group_Role_ID]",
-                "Group_ID_Table.[Group_ID]",
-                "Group_ID_Table.[Group_Name]",
-                "Group_ID_Table.[Group_Type_ID]"
-            ])
-            .withFilter(filter)
-            .fromTable(table)
-            .get()
-            .then(response => {
-                return response.data.map(group => {
-                    return {
-                        id: group.Group_ID,
-                        name: group.Group_Name,
-                        role: group.Group_Role_ID,
-                        type: group.Group_Type_ID
-                    };
-                });
-            });
-    }
+  public setCongregation(HouseholdID: number, SiteID: number): Promise<ISite> {
+    const mp = new MP();
+    return mp
+      .fromTable("households")
+      .put([
+        {
+          Household_ID: HouseholdID,
+          Congregation_ID: SiteID
+        }
+      ])
+      .then(response => {
+        return {
+          id: response.data[0].Congregation_ID,
+          name: response.data[0].Congregation_Name
+        };
+      });
+  }
 
-    public setCongregation(HouseholdID: number, SiteID: number): Promise<ISite> {
-        const mp = new MP();
-        return mp
-            .fromTable('households')
-            .put([
-                {
-                    Household_ID: HouseholdID,
-                    Congregation_ID: SiteID
-                }
-            ])
-            .then(response => {
-                return {
-                    id: response.data[0].Congregation_ID,
-                    name: response.data[0].Congregation_Name
-                };
-            })
-    }
+  public getLifeStage(UserID: number): Promise<ILifeStage> {
+    const db = this.mongo.client.db("personalization");
+    const collection = db.collection("users");
+    return collection
+      .findOne({ userId: UserID }, { lifeStage: true })
+      .then(document => {
+        if (!document) return null;
+        return document.lifeStage;
+      });
+  }
 
-    public getLifeStage(UserID: number): Promise<ILifeStage> {
-        const db = this.mongo.client.db('personalization');
-        const collection = db.collection('users');
-        return collection.findOne({ userId: UserID }, { lifeStage: true })
-            .then((document) => {
-                if (!document) return null;
-                return document.lifeStage
-            })
-    }
+  public setLifeStage(
+    UserID: number,
+    lifeStage?: ILifeStage
+  ): Promise<ILifeStage> {
+    const db = this.mongo.client.db("personalization");
+    const collection = db.collection("users");
+    return collection
+      .updateOne(
+        { userId: UserID },
+        { $set: { lifeStage: lifeStage } },
+        { upsert: true }
+      )
+      .then(document => {
+        return this.getLifeStage(UserID);
+      });
+  }
 
-    public setLifeStage(UserID: number, lifeStage?: ILifeStage): Promise<ILifeStage> {
-        const db = this.mongo.client.db('personalization');
-        const collection = db.collection('users');
-        return collection.updateOne({ userId: UserID }, { $set: { lifeStage: lifeStage } }, { upsert: true })
-            .then((document) => {
-                return this.getLifeStage(UserID);
-            });
-    }
-
-    public getContactDetails(ContactID: number): Promise<IContact> {
-        const filter = `Contacts.[Contact_ID] = ${ContactID}`;
-        const table = `Contacts`;
-        const mp = new MP();
-        return mp
-            .fromTable(table)
-            .withFilter(filter)
-            .get()
-            .then(response => {
-                return {
-                    nickName: response.data[0].Nickname,
-                    firstName: response.data[0].First_Name
-                }
-            })
-    }
+  public getContactDetails(ContactID: number): Promise<IContact> {
+    const filter = `Contacts.[Contact_ID] = ${ContactID}`;
+    const table = `Contacts`;
+    const mp = new MP();
+    return mp
+      .fromTable(table)
+      .withFilter(filter)
+      .get()
+      .then(response => {
+        return {
+          nickName: response.data[0].Nickname,
+          firstName: response.data[0].First_Name
+        };
+      });
+  }
 }
