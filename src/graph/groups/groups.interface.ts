e7a33bb2e7e91f4881e9d9fa9b8f3ead7d1f2108
j-brammer <jbrammer@callibrity.com>
2019-09-10 09:51:32 -0400

    added interfaces for role,type,meeting

--- src/graph/groups/groups.interface.ts
+++ src/graph/groups/groups.interface.ts
@@ -1,8 +1,23 @@
 export interface IGroup {
     id: number
     name: string
-    meeting: string
-    role: string
-    type: number
+    meeting: Meeting
+    role: Role
+    type: Type
 }
 
+export interface Meeting {
+    day: string
+    time: string
+    frequency: string
+}
+
+export interface Role {
+    id: number
+    name: string
+}
+
+export interface Type {
+    id: number
+    name: string
+}
